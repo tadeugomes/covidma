7b9063c9f0e8146d4085d1a8d746ae1f4a35a419
Tadeu Gomes <32282635+tadeugomes@users.noreply.github.com>
2020-08-15 21:43:57 -0300

    Add files via upload

---
+++ taxa_diaria_covid_ma.R
@@ -0,0 +1,25 @@
+library(lubridate)
+library(openair)  #função calendarPlot
+library(zoo) # moving averages     
+library(tidyverse)
+
+#leitura do dataset
+caso <- read.csv("~/Downloads/caso.csv")
+
+#filtrar Maranhão
+ma<-caso %>% 
+  filter(state == "MA")
+#transformar coluna de string para data 
+ma$date<-as_date(ma$date)
+
+#Transformar/calcular taxa de mortes 
+ma<-ma %>% 
+  mutate(taxa_mortes = round((deaths / confirmed)*100, digits = 4))
+
+#Taxa diária de mortes. Mortes/casos confirmados. digits = 4
+calendarPlot(ma, pollutant = "taxa_mortes",
+             main = "Evolução da taxa diária de mortes por Covid-19 no Maranhão (em %)", 
+             xlab ="Elaboração: @teixeira_tadeu. Dados: www.brasil.io")
+
+
+#Calcular média movel
